55103dcba5cc0032f63f0fa9eec7adbeff4d50f9
nicholasmyllari <nicholas.myllari@aalto.fi>
2017-03-06 14:08:27 +0200

    ready

---
+++ slideshow.js
@@ -0,0 +1,65 @@
+var playing = true;
+var news = null;
+if (localStorage.current) {
+	localStorage.current = Number(localStorage.current);
+} else {
+	localStorage.current = 0;
+}
+
+var timer = window.setInterval(function(){naturalNext();},3000);
+
+function setNews() {
+	$('.news').fadeOut(400, function() {
+        $('#newsHeadline').html(news[localStorage.current].otsikko);
+   	    $('#newsContent').html(news[localStorage.current].sisältö);
+   	    $('#newsDate').html(news[localStorage.current].päivämäärä);
+	});
+    $('.news').fadeIn(400);
+}
+function togglePauseText() {
+	if (playing) {
+		document.getElementById("pause").textContent = "pysäytä";
+	} else {
+		document.getElementById("pause").textContent = "jatka";
+	}
+}
+window.onload = function() {
+	$.getJSON("https://myllarn1-5488c.firebaseio.com/uutiset.json", function (data) {
+    console.log(data);
+    news = data;
+    setNews();
+  });
+};
+function playPause() {
+	if (playing) {
+		playing = false;
+		clearInterval(timer);
+	} else {
+		playing = true;
+		timer = window.setInterval(function(){naturalNext();},3000); 
+	}
+	togglePauseText();
+}
+
+function naturalNext() {
+	localStorage.current ++;
+	localStorage.current = localStorage.current % 3;
+    setNews();
+}
+
+function next() {
+	playing = false;
+	clearInterval(timer);
+	naturalNext();
+	togglePauseText();
+}
+
+function prev() {
+	playing = false;
+	clearInterval(timer);
+	localStorage.current ++;
+	localStorage.current ++;
+	localStorage.current = localStorage.current % 3;
+    setNews();
+    togglePauseText();
+}
